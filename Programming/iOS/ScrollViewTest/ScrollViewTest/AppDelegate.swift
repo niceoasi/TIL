17c8fb026a97d98f955ffcfef8e3a91aadba189e
Ethan <nicebuvr@naver.com>
2021-03-26 01:52:44 +0900

    Test Scroller UI

---
+++ Programming/iOS/ScrollViewTest/ScrollViewTest/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ScrollViewTest
+//
+//  Created by Ethan on 2021/03/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
